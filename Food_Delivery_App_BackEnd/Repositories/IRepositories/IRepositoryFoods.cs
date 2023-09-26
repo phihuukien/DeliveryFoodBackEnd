5a7085daf1b66a1f35fa8812316d0505781e6dac
dattai2k2 <dattai2k2@gmai.com>
2023-09-26 23:38:22 +0700

    tai comit

--- Food_Delivery_App_BackEnd/Repositories/IRepositories/IRepositoryFoods.cs
+++ Food_Delivery_App_BackEnd/Repositories/IRepositories/IRepositoryFoods.cs
@@ -1,9 +1,16 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Food_Delivery_App_BackEnd.ModelDTO;
+using Food_Delivery_App_BackEnd.Models.DataModels;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Food_Delivery_App_BackEnd.Repositories.IRepositories
 {
     public interface IRepositoryFoods
     {
         public JsonResult GetOneFoodById(string foodId);
+        public JsonResult GetAllFoodByRestaurantId(string restaurantId);
+        public JsonResult GetRestaurantsByNameFood(string name);
+        public IActionResult AddFood(Foods food);
+        public IActionResult UpdateFood(Foods food);
+        public IActionResult DelelteFood(String id);
     }
 }
